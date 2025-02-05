9a2b501f77f3098cb44081a8effdaa719d0f8402
Satya Prakash Mahour <99254801+Satya-100@users.noreply.github.com>
2025-02-05 11:53:15 +0530

    Time: 32 ms (30.43%), Space: 17.3 MB (52.8%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,35 +1,31 @@
 class Solution {
-    void dfs(int row, int col, vector<vector<char>>& grid, vector<pair<int, int>>& temp, vector<vector<int>>& vis, int nrow, int ncol){
-        int m = grid.size();
-        int n = grid[0].size();
-        vis[row][col]=1;
-        temp.push_back({row-nrow, col-ncol});
-        
-        int crow[] = {-1, 0, 1, 0};
-        int ccol[] = {0, -1, 0, 1};
-        for(int i=0;i<4;i++){
-            int trow = row + crow[i];
-            int tcol = col + ccol[i];
-            if(trow>=0 && trow<m && tcol>=0 && tcol<n && !vis[trow][tcol] && grid[trow][tcol]=='1'){
-                dfs(trow, tcol, grid, temp, vis, nrow, ncol);
-            }
-        }
+    void traverse(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
+        int m=grid.size();
+        int n=grid[0].size();
+        if(row<0 || row>=m || col<0 || col>=n)  return ;
+        if(grid[row][col]!='1') return ;
+        if(vis[row][col])   return ;
+        vis[row][col] = 1;
+
+        traverse(row, col+1, vis, grid);
+        traverse(row, col-1, vis, grid);
+        traverse(row+1, col, vis, grid);
+        traverse(row-1, col, vis, grid);
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<pair<int, int>>> st;
-        vector<vector<int>> vis(m, vector<int>(n,0));
+        int m=grid.size();
+        int n=grid[0].size();
+        int island=0;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(!vis[i][j] && grid[i][j]=='1'){
-                    vector<pair<int, int>> temp;
-                    dfs(i, j, grid, temp, vis, i, j);
-                    st.push_back(temp);
+                    traverse(i, j, vis, grid);
+                    island++;
                 }
             }
         }
-        return st.size();
+        return island;
     }
 };
