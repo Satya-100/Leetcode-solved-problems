1ba899146ebf2dd2f83dde9576a0573aeef9890a
Satya Prakash Mahour <99254801+Satya-100@users.noreply.github.com>
2025-04-11 21:33:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (68.97%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,23 +1,13 @@
-#include <cmath>
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long k=x;
-        long long r = 0;
-        if (x < 0)
-            return false;
-        else {
-            // int k = x;
-            while (k > 0) {
-                long long rem=k%10;
-                r = r*10 + rem;
-                // i--;
-                k /= 10;
-            }
-            if (x == r)
-                return true;
-            else
-                return false;
+        if(x<0) return false;
+        int temp = x;
+        long k=0;
+        while(temp>0){
+            k = (k*10) + (temp%10);
+            temp/=10;
         }
+        return k==x;
     }
 };
