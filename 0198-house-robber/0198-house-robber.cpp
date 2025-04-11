b52816f333f14ef11e22b4d8484c2ef23c7c4ae1
Satya Prakash Mahour <99254801+Satya-100@users.noreply.github.com>
2025-04-11 12:33:31 +0530

    Time: 0 ms (100%), Space: 10.1 MB (78.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -14,7 +14,22 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n, -1);
-        return f(n-1, nums, dp);
+        // vector<int> dp(n, 0);
+        // dp[0] = nums[0];
+        int prev2 = 0;
+        int prev = nums[0];
+        int curr;
+
+        for(int i=1;i<n;i++){
+            int take = nums[i] + prev2;
+            int notTake = prev;
+
+            curr = max(take, notTake);
+
+            prev2 =  prev;
+            prev = curr;
+        }
+
+        return prev;
     }
 };
