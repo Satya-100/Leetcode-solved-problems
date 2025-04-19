59b6871812c56a33bd0d27191fd1fa5181449646
Satya Prakash Mahour <99254801+Satya-100@users.noreply.github.com>
2025-04-19 11:34:29 +0530

    Time: 0 ms (100%), Space: 24.4 MB (85.46%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
@@ -4,11 +4,25 @@ public:
         int n = nums.size();
         int res = 0;
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i] + nums[j] < target){
-                    res++;
-                }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i] + nums[j] < target){
+        //             res++;
+        //         }
+        //     }
+        // }
+
+        sort(nums.begin(), nums.end());
+        int left = 0, right = n-1;
+        
+        while(left!=right){
+            if(nums[left] + nums[right] < target){
+                res+=right-left;
+                left++;
+            }
+
+            else{
+                right--;;
             }
         }
 
