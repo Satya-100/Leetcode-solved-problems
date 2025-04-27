cf65995bf03959d76dee379590f72120b24a6b46
Satya Prakash Mahour <99254801+Satya-100@users.noreply.github.com>
2025-04-27 21:29:23 +0530

    Time: 55 ms (21.2%), Space: 90.9 MB (7.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,8 +4,9 @@ public:
         int n=nums.size();
         unordered_set<int> st;
         for(int i=0;i<n;i++){
+            if(st.find(nums[i])!=st.end())  return true;
             st.insert(nums[i]);
         }
-        return st.size()<nums.size()?true:false;
+        return false;
     }
 };
